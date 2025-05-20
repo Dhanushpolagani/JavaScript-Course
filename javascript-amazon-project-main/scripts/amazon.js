1a3e2ffded8de8b5b4eef46ac5db66c98603b519
Dhanushpolagani <mailtodhanush2004@gmail.com>
2025-05-20 13:24:17 +0530

    make imake the cart quantity interactive

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -58,7 +58,7 @@ console.log(productsHTML);
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 document.querySelectorAll('.js-add-to-cart').forEach((button) => {
     button.addEventListener('click', () => {
-        const productId = button.dataset.id;
+        const productId = button.dataset.productId;
         // const quantity = Number(quantitySelector.value);
 
         let matchingItem;
@@ -76,6 +76,17 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
             quantity: 1 
         });
     }
+
+
+    let cartQuantity=0;
+
+    cart.forEach((item) =>{
+        cartQuantity += item.quantity;
+    });
+
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+
+    console.log(cartQuantity);
         console.log(cart);
     });
 });
