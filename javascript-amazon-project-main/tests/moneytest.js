6226d3d02070316d86052798e098035518a7c578
Dhanushpolagani <mailtodhanush2004@gmail.com>
2025-05-21 18:42:59 +0530

    create automated tests for formatCurrency

---
+++ javascript-amazon-project-main/tests/moneytest.js
@@ -0,0 +1,28 @@
+import {formatCurrency} from '../scripts/utils/money.js';
+
+console.log('test suite : formatCurrency');     
+
+console.log('converts cents into dollars');
+
+if(formatCurrency(2095) === '20.95'){
+    console.log('Test passed');
+} else{
+    console.log('failed');
+}
+
+console.log('works with 0');
+
+if(formatCurrency(0)==='0.00'){
+    console.log('test passed');
+
+} else{
+    console.log('failed');
+}
+
+console.log('rounds to the nearest cent');
+
+if( formatCurrency(20.91)){
+    console.log('passed');
+} else{
+    console.log('failed');
+}
