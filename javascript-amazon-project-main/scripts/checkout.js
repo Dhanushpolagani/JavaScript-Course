f12fac2e3e67b77e0645fc93fb7efec8b597a7bd
Dhanushpolagani <mailtodhanush2004@gmail.com>
2025-05-21 17:38:41 +0530

    save data and generate HTML for payment summary

--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -1,3 +1,4 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
-
+import { renderPaymentSummary } from './checkout/payementSummary.js';
 renderOrderSummary();
+renderPaymentSummary();
--- javascript-amazon-project-main/scripts/checkout/orderSummary.js
+++ javascript-amazon-project-main/scripts/checkout/orderSummary.js
@@ -1,9 +1,9 @@
 import { cart, removeFromCart ,updateDeliveryOption} from '../../data/cart.js';
-import { products } from '../../data/products.js';
+import { products, getProduct} from '../../data/products.js';
 import { formatCurrency } from '../utils/money.js';
 import { hello } from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
-import { deliveryOptions } from '../../data/deliveryOptions.js';
+import { deliveryOptions,getDeliveryOption } from '../../data/deliveryOptions.js';
 
 hello();
 
@@ -17,17 +17,13 @@ cart.forEach((cartItem) => {
   const productId = cartItem.productId;
 
   // Find the matching product
-  let matchingProduct;
-  products.forEach((product) => {
-    if (product.id === productId) {
-      matchingProduct = product;
-    }
-  });
+  const matchingProduct = getProduct(productId);
 
 
   // Find selected delivery option
   const deliveryOptionId = cartItem.deliveryOptionId;
-  let deliveryOption;
+
+let deliveryOption = getDeliveryOption(cartItem.deliveryOptionId);
   deliveryOptions.forEach((option) => {
     if (option.id === deliveryOptionId) {
       deliveryOption = option;
