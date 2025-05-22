cb60a72bb91c0c9c35496957d99d07e7e5398651
Dhanushpolagani <mailtodhanush2004@gmail.com>
2025-05-22 17:49:52 +0530

    convert products into classes

--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -1,6 +1,6 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from './checkout/payementSummary.js';
-import cart from "../data/cart-class.js";
+//import cart from "../data/cart-class.js";
 
 renderOrderSummary();
 renderPaymentSummary();
