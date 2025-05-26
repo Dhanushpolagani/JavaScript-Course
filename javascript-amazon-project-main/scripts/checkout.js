c128726d07ca104f005cd76f21915a2a17865a23
Dhanushpolagani <mailtodhanush2004@gmail.com>
2025-05-26 23:35:12 +0530

    load products from the backend

--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -1,7 +1,9 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from './checkout/payementSummary.js';
+import { loadProducts } from "../data/products.js";
 //import cart from "../data/cart-class.js";
-import '../data/backend-practice.js';
-
+//import '../data/backend-practice.js';
+loadProducts(() =>{
 renderOrderSummary();
-renderPaymentSummary();
+renderPaymentSummary();   
+}); 
